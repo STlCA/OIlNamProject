f986fca6fc1c1f5422d054fb946b256a9882f893
STIKA <graysky9@naver.com>
2024-07-10 04:29:54 +0900

    feat : 스토리기본틀

---
+++ Assets/2.Scripts/UI/SceneEffect.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneEffect : MonoBehaviour
+{
+    public Image startfadeImage = null;
+    public Image fadeImage = null;
+    public Image crackingImage = null;
+    public Image textBackImage = null;
+
+    private float fadeTime = 1f;
+    private float waitTime = 1f;
+    private float time = 0f;
+
+    private float circleScale = 100f;
+
+    public TMP_Text storyText = null;//임시public
+
+    public void StorySceneOn()
+    {
+        StartCoroutine("StartFadeFlow");
+    }
+
+    public IEnumerator StartFadeFlow()
+    {
+        startfadeImage.gameObject.SetActive(true);
+
+        while (startfadeImage.rectTransform.localScale.x < circleScale)
+        {
+            time += Time.deltaTime / fadeTime;
+            float x = Mathf.Lerp(0, circleScale, time);
+            float y = Mathf.Lerp(0, circleScale, time);
+
+            Vector3 temp = new Vector3(x, y, 1);
+
+            startfadeImage.rectTransform.localScale = temp;
+
+            yield return null;
+        }
+
+        fadeImage.gameObject.SetActive(true);
+        startfadeImage.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(1);
+
+        crackingImage.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine("TypeTextEffect");
+    }
+
+    public IEnumerator FadeFlow()
+    {
+        fadeImage.gameObject.SetActive(true);
+        Color alpha = fadeImage.color;
+        time = 0f;
+
+        Time.timeScale = 0.0f;
+
+        while (alpha.a < 1f)
+        {
+            time += Time.deltaTime / fadeTime;
+            alpha.a = Mathf.Lerp(0, 1, time);
+            fadeImage.color = alpha;
+            yield return null;
+        }
+
+        time = 0f;
+
+        Time.timeScale = 1f;
+
+        yield return new WaitForSeconds(waitTime);
+
+        while (alpha.a > 0f)
+        {
+            time += Time.deltaTime / fadeTime;
+            alpha.a = Mathf.Lerp(1, 0, time);
+            fadeImage.color = alpha;
+            yield return null;
+        }
+
+        fadeImage.gameObject.SetActive(false);
+    }
+
+    public void TypeEffectON(TMP_Text text)
+    {
+        storyText = text;
+    }
+
+    public IEnumerator TypeTextEffect(/*string text*/)
+    {
+        textBackImage.gameObject.SetActive(true);
+
+        storyText.text = string.Empty;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        string text = "콰지직.";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            stringBuilder.Append(text[i]);
+            storyText.text = stringBuilder.ToString();
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        text = "\n\n전 세계에 균열이 발생했다.";
+        for (int i = 0; i < text.Length; i++)
+        {
+            stringBuilder.Append(text[i]);
+            storyText.text = stringBuilder.ToString();
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
