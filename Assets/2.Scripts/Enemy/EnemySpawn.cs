8bd124ac2b2b5b7b35585042c8f56a03acfba0ff
STIKA <graysky9@naver.com>
2024-08-04 21:33:53 +0900

    feat  : 전부삭제 필살기

--- Assets/2.Scripts/Enemy/EnemySpawn.cs
+++ Assets/2.Scripts/Enemy/EnemySpawn.cs
@@ -208,11 +208,12 @@ public class EnemySpawn : MonoBehaviour
 
     public void LethalAttack()
     {
-        for (int i = 0; i < enemyList.Count; i++) // 버그찾음 제거하면서 인덱스 땅겨져서 그럼
+        int temp = enemyList.Count;
+        for (int i = 0; i < temp; i++) // 버그찾음 제거하면서 인덱스 땅겨져서 그럼
         {
-            Enemy enemy = enemyList[i];
+            Enemy enemy = enemyList[0];
             enemy.SpecialAttacked();
             Debug.Log("필살기 쥬금 " + i);
-        }        
+        }
     }
 }
